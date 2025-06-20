7474e2b9b94047d556adb0fc046debe75a207df3
Prejan <neupaneprejan2018@gmail.com>
2025-06-20 16:53:19 +0545

    allowed all host

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,8 @@ SECRET_KEY = 'django-insecure-6h6n)e&p9ht$@e_qf*yg*rf*t(dd5oa(3ap034%d(#sa5(rc)@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+ALLOWED_HOSTS = ['*']
+
 
 
 
