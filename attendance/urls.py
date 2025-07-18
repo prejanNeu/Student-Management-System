f6c3c426a39d43e0175116378190b25b31e63cc7
Prejan <neupaneprejan2018@gmail.com>
2025-07-19 00:37:58 +0545

    api method is post

--- attendance/urls.py
+++ attendance/urls.py
@@ -4,7 +4,8 @@ from .views import (
     mark_attendance, 
     class_list, 
     subject_list,
-    get_student_by_class
+    get_student_by_class,
+    get_attendance_detail_by_id
 )
 
 
@@ -13,5 +14,6 @@ urlpatterns = [
     path("api/mark_attendance/", mark_attendance, name="mark_attendance"),
     path("api/class_list/", class_list, name="class_list"),
     path("api/subject_list/<int:classlevel>/",subject_list, name="subject_list"),
-    path("api/get_student_by_class/", get_student_by_class, name="get_student_by_class"),
+    path("api/get_student_by_class/<int:classlevel>", get_student_by_class, name="get_student_by_class"),
+    path("api/get_attendance_detail_by_id/<int:id>/",get_attendance_detail_by_id,name="get_attendance_detail_by_id"),
 ]
--- attendance/views.py
+++ attendance/views.py
@@ -4,14 +4,13 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from drf_yasg.utils import swagger_auto_schema
 from .models import Attendance 
-from account.models import StudentClassEnrollment
+from account.models import StudentClassEnrollment, CustomUser
 from rest_framework import status 
 from rest_framework.permissions import IsAuthenticated
 from .serializers import AttendanceSerializer, StudentSerializer
 from django.db import IntegrityError
 from account.models import ClassLevel, ClassSubject, Subject
 from attendance.serializers import ClassLevelSerializer, SubjectOnlySerializer
-
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
@@ -115,10 +114,10 @@ def subject_list(request, classlevel):
 
 @api_view(["GET"])
 def get_student_by_class(request, classlevel):
-    if request.role == "teacher" or request.role == "admin":
+    if request.user.role == "teacher" or request.user.role == "admin":
         students = []
 
-        objs = StudentClassEnrollment.objects.filter(classlevel_id=classlevel, is_current=True)
+        objs = StudentClassEnrollment.objects.filter(class_level_id=classlevel, is_current=True)
 
         for obj in objs:
             student = obj.student
@@ -130,6 +129,44 @@ def get_student_by_class(request, classlevel):
     return Response({"message": "You are not teacher or admin"}, status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(["GET"])
+def get_attendance_detail_by_id(request, id):
+    # if request.user.role not in ["teacher", "admin"]:
+    #     return Response(
+    #         {"message": "You are not authorized to view attendance details."},
+    #         status=status.HTTP_403_FORBIDDEN
+    #     )
 
+    # Get the user
+    obj = CustomUser.objects.filter(id=id).first()
+
+    if not obj:
+        return Response(
+            {"message": "Student not found."},
+            status=status.HTTP_404_NOT_FOUND
+        )
+
+    if obj.role != "student":
+        return Response(
+            {"message": "Invalid user role. Expected a student."},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+    studentclass = StudentClassEnrollment.objects.filter(student=obj, is_current=True).first()
+    # Get the student's current class level
+    classlevel = studentclass.class_level
+
+    if not classlevel:
+        return Response(
+            {"message": "Current class level not found for this student."},
+            status=status.HTTP_404_NOT_FOUND
+        )
+
+    # Get attendance data
+    attendance_data = Attendance.objects.filter(student=obj, classlevel=classlevel)
+
+    serializer = AttendanceSerializer(attendance_data, many=True)
+
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
         
