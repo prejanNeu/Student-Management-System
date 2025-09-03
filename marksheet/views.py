baf621ef74486814ccf371cf7ff1a4bc44392a00
Prejan <neupaneprejan2018@gmail.com>
2025-09-03 23:37:09 +0545

    issue solve

--- marksheet/views.py
+++ marksheet/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from django.db.models import Avg, Max, Min, ExpressionWrapper, FloatField
 from rest_framework import status
 from django.db.models import Q
 from .serializers import (
